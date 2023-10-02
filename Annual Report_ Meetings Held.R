e43c9b5d2cb67a66015e0dfaeaf426450482c4d0
katiearzu <145294195+katiearzu@users.noreply.github.com>
2023-10-01 21:50:03 -0400

    Add files via upload

---
+++ Annual Report_ Meetings Held.R
@@ -0,0 +1,13 @@
+# Create the data for the chart
+H <- c(1157,1512,2193,1932,612)
+M <- c("Manhattan","Bronx","Brooklyn","Queens","Staten Island")
+
+# Give the chart file a name
+png(file = "Annual_report_meetingshelp.png")
+
+# Plot the bar chart 
+barplot(H,names.arg=M,xlab="Boroughs",ylab="Total IEP Meetings Held",col="blue",
+        main="SY'20-'21 Annual Report: IEP Meetings (Eligible & Ineligible",border="red")
+
+# Save the file
+dev.off()
