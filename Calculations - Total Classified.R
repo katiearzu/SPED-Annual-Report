1b816bb2f708e3e0bffff2cbcbbe1a1c80635678
katiearzu <145294195+katiearzu@users.noreply.github.com>
2023-10-20 22:09:23 -0400

    Add files via upload

---
+++ Calculations - Total Classified.R
@@ -0,0 +1,17 @@
+# Create a vector. 
+y <- c(93,426,172,90,76,119,120,229,249,350,259,116,133,171,409,60,123,6,147,321,196,193,6,354,208,211,365,242,172,200,571,69)
+
+# Find Mean.
+result.mean <- mean(y)
+print(result.mean)
+
+# Find the median.
+median.result <- median(y)
+print(median.result)
+
+#Find the standard deviation.
+s<-sd(y)
+print(s)
+
+#Find the variance
+var(y)
---
+++ Calculations - Total Ineligible.R
@@ -0,0 +1,18 @@
+# Create a vector. 
+x <- c(12,82,37,20,12,18,19,27,30,56,38,19,22,25,56,8,12,6,19,23,16,13,5,29,21,28,31,27,19,25,41,6)
+
+  
+# Find Mean.
+result.mean <- mean(x)
+print(result.mean)
+
+# Find the median.
+median.result <- median(x)
+print(median.result)
+
+#Find the standard deviation.
+s<-sd(x)
+print(s)
+
+#Find the variance
+var(x)
